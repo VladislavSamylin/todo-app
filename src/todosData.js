8a98268d3003ac1d45c8bcb96844062fb4072a18
JustDoEat <mr.verdy@mail.ru>
2021-12-08 16:09:30 +0300

    commit 1

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Вынести мусор",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Сходить в магазин",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Убраться в квартире",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Поиграть с Феней",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Поесть",
+        completed: false
+    }
+]
+
+export default todosData;
